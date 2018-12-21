fc9e9efa0722789f72fb8f30c1d6550e4355f652
tania <strekasia@tut.by>
2018-12-21 17:22:32 +0300

    feat: add button to show details

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    counters = [];
+    showFlag = true;
+
+    onButtonClick() {
+        this.showFlag = !this.showFlag;
+        this.counters.push(this.counters.length + 1);
+    }
+}
